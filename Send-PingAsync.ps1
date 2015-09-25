96dbc49c93c75b2df7779ee8de1d734216ec7688
Jesse Davis <secabstraction@users.noreply.github.com>
2015-09-25 08:17:44 -0500

    Create Send-PingAsync.ps1

---
+++ Send-PingAsync.ps1
@@ -0,0 +1,22 @@
+function Send-PingAsync {
+
+[CmdLetBinding()]
+     Param(
+        [Parameter(Position = 0, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String[]]$ComputerName
+    ) #End Param
+
+    $Pings = New-Object Collections.Arraylist
+
+    foreach ($Computer in $ComputerName) {
+        [void]$Pings.Add((New-Object Net.NetworkInformation.Ping).SendPingAsync($Computer, 250))
+    }
+    [Threading.Tasks.Task]::WaitAll($Pings)
+
+    foreach ($Ping in $Pings) {
+        if ($Ping.Result.Status -eq [Net.NetworkInformation.IPStatus]::Success) {
+            Write-Output $Ping.Result
+        }
+    }
+}
