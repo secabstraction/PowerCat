57be1cd2f631320474b71d0b816c739087248bb3
Jesse Davis <secabstraction@users.noreply.github.com>
2015-09-25 16:35:08 -0500

    Update Icmp.ps1

--- Icmp.ps1
+++ Icmp.ps1
@@ -1,7 +1,7 @@
 $IcmpListener = New-Object Net.Sockets.Socket([Net.Sockets.AddressFamily]::InterNetwork, [Net.Sockets.SocketType]::Raw, [Net.Sockets.ProtocolType]::Icmp)
-$IcmpListener.Bind(New-Object IPEndPoint(IPAddress.Parse("10.1.1.2"), 0));
+$IcmpListener.Bind((New-Object Net.IPEndPoint([Net.IPAddress]::Parse("10.10.10.10"), 0)))
 $IcmpListener.IOControl([Net.Sockets.IOControlCode]::ReceiveAll, [byte[]]@(1, 0, 0, 0), [byte[]]@(1, 0, 0, 0));
 
-$Buffer = New-Object byte[](4096)
+$Buffer = New-Object byte[](128)
 $RemoteEndPoint = New-Object Net.IPEndPoint([Net.IPAddress]::Any, 0)
 $BytesRead = $IcmpListener.ReceiveFrom($Buffer, [ref]$RemoteEndPoint)
