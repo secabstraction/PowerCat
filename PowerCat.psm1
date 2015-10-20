e4211568c4be98b16125942bb084c672d7cac531
Jesse Davis <secabstraction@users.noreply.github.com>
2015-10-20 08:50:05 -0500

    Update PowerCat.psm1

--- PowerCat.psm1
+++ PowerCat.psm1
@@ -1,1 +1,3 @@
-"Coming Soon!"
+Write-Warning 'PowerCat is under construction. Check back soon for updates.'
+Write-Warning 'I will remove this warning when the code is functional.'
+Write-Warning 'If you want to follow along, check out the working branch.'
